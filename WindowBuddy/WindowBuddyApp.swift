682828c327403f7f74e2aca59c87f4e8603caf65
hreinssondev <hreinssondev@gmail.com>
2026-05-30 12:43:44 +0200

    Initial Commit

---
+++ WindowBuddy/WindowBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowBuddyApp.swift
+//  WindowBuddy
+//
+//  Created by H on 30/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WindowBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
